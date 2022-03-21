c6730ade480d0f9b69c5fe8c82ffda26ea27a714
Nay-nazarova <102009758+Nay-nazarova@users.noreply.github.com>
2022-03-21 10:47:33 +0400

    Update and rename first = int(56).py to first = int.py

--- first = int(56).py
+++
@@ -1,4 +0,0 @@
-first = int(56)
-second = int(input('Введите число: '))
-result = first + second
-print(result)
---
+++ first = int.py
@@ -0,0 +1,4 @@
+first = int(56)
+second = int(input('Nay: '))
+result = first + second
+print(result)
